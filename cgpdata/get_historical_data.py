d49d7745261d441f7b66dcaa16be6f2df204d337
Hugh Harford <hugh.harford@poscoconsulting.com>
2025-03-11 19:22:15 +0000

    Create backwards collecting by month from AlphaVantage
    include check on if run out of Free API which then gives a number of yesterdays data returns before cutting off, or gives short information on how to get unlimited calls

--- cgpdata/get_historical_data.py
+++ cgpdata/get_historical_data.py
@@ -4,47 +4,77 @@ import os
 import requests
 import pandas as pd
 
-AIRFLOW_HOME = os.getenv("AIRFLOW_HOME")
+from google.cloud import storage
 
+
+AIRFLOW_HOME = os.getenv("AIRFLOW_HOME")
 KEY2 = os.getenv("HSTH_AV_KEY")
 
+SOURCE="alphavantage"
+API_KEY = KEY2
+YYYY="2022"
+MM="13" # start from 13 to get the full year
+INTERVAL="5min"
+TICKER="SPY"
 
-def get_historicals():
-    print("looking to get data")
-    API_KEY = KEY2
-    # url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=SPY&interval=5min&apikey={API_KEY}"
-    url = f"https://www.alphavantage.co/query?function=HISTORICAL_OPTIONS&symbol=IBM&date=2017-11-15&apikey={API_KEY}"
+BUCKET_NAME = "cgpdata"
+GCPBUCKET_FOLDER_STRING = "hist"
+LOCAL_PATH="/home/hugh.harford/code/hughharford/cgpdata/data"
+YESTERDAY=(datetime.today()- timedelta(1)).strftime('%Y-%m-%d')
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket:        bucket_name = "your-bucket-name"
+    # The path to your file to upload:  source_file_name = "local/path/to/file"
+    # The ID of your GCS object:        destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+def get_historicals_from_alphavantage(ticker=TICKER, interval=INTERVAL, yyyy=YYYY, mm=MM):
+    print(f"getting Monthly data from {SOURCE} for {yyyy}_{mm}")
+
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval={interval}&month={yyyy}-{mm}&outputsize=full&apikey={API_KEY}"
     response = requests.get(url)
     data = response.json()
+    return data
+    # return 1
 
-    df = pd.DataFrame.from_dict(data["Time Series"], orient="index")
-    print(df.head())
-    df.to_csv("data/test-2017-11-15.csv")
+def source_backward_months_data():
+    yyyy=int(YYYY)
+    mm=int(MM)
+
+    bucket_name = BUCKET_NAME
+    gcpbucket_folder_string = GCPBUCKET_FOLDER_STRING
 
 
-# def fetch_and_upload_sp500():
-#     """Fetch S&P 500 data using yfinance and upload it to GCS"""
-#     # GCS details
-#     gcs_hook = GCSHook(gcp_conn_id='google_cloud_default')
-#     bucket_name = "cgpdata"  # GCS landing bucket name
-#     object_name = f"s&p500_data_{datetime.today().strftime('%Y%m%d')}.csv"  # File name based on date
-
-#     # Fetch the S&P 500 data using yfinance
-#     sp500_data = yf.download('^GSPC', period='1d', interval='1d')
-
-#     # Convert DataFrame to CSV in-memory
-#     csv_buffer = io.StringIO()
-#     sp500_data.to_csv(csv_buffer)
-#     csv_buffer.seek(0)
-
-#     # Upload the CSV data to Google Cloud Storage
-#     gcs_hook.upload(
-#         bucket_name=bucket_name,
-#         object_name=object_name,
-#         data=csv_buffer.getvalue(),
-#         mime_type='text/csv'
-#     )
-#     print(f"Successfully uploaded {object_name} to GCS bucket {bucket_name}")
+    for m in range(mm,1,-1):
+        mm=mm-1
+        print(f'yyyy={str(yyyy)} and mm={str(mm)}')
+        data = get_historicals_from_alphavantage(yyyy=yyyy, mm=mm)
+        df = pd.DataFrame.from_dict(data["Time Series (5min)"], orient="index")
+        csv_name = f"{yyyy}-{mm}-{TICKER}_{INTERVAL}_{SOURCE}.csv"
+        df.to_csv(f"{LOCAL_PATH}/{csv_name}")
+        if df["Time Series (5min)"][0][:10] != YESTERDAY:
+            source_file_name = f"{LOCAL_PATH}/{csv_name}"
+            destination_blob_name = f"{gcpbucket_folder_string}/{csv_name}"
+            upload_blob(bucket_name, source_file_name, destination_blob_name)
 
 if __name__ == "__main__":
-    get_historicals()
+    source_backward_months_data()
